9feeb90113592c85977e5277ba45a28e8ba8b2a7
UCAS-Sawyer <sawyer.wood@ucas-edu.net>
2025-04-24 13:44:01 -0600

    stuff

--- song_handling.py
+++ song_handling.py
@@ -1,13 +1,34 @@
 # Sawyer Wood
 
+from save_load import load_to_playlists as load
+from save_load import songs_to_save as save
+
+
+
 def add_song():
     # Function to add a song to the playlist
+    filepath = input("Enter the file path of the song to add: ")
+    name = input("Enter the name of the song: ")
+
 
 def remove_song():
     # Function to remove a song from the playlist
+    pass
 
 def add_playlist():
     # Function to add a playlist
+    pass
 
 def remove_playlist():
     # Function to remove a playlist
+    pass
+
+
+
+#Playlists format:
+#playlists = {
+#    playlist1name = [[song1name, song1path], [song2name, song2path], ...],
+#    playlist2name = [[song1name, song1path], [song2name, song2path], ...]
+#}
+
+print(load("songs.csv"))
