4688017529a80c28970b9ceb9d95c5b10b8a62f1
3334gFIOP-school <jackson.hauley@ucas-edu.net>
2025-04-24 13:13:06 -0600

    eeeeee

--- audio.py
+++ audio.py
@@ -5,25 +5,29 @@ import pygame
 from pydub import AudioSegment
 
 def play_song(pse_ply, file_path):
-    pygame.mixer.init()
-    pygame.mixer.music.load(file_path)  # Parameter for file path
-    pygame.mixer.music.play()
-    current_text = pse_ply["text"]
-    if current_text == "▶":
-        # Adjust font size for "Pause" to make it visually similar to "Play"
-        pse_ply.config(text="⏸", font=("Helvetica", 20, "bold"))  
-        try:
-            pygame.mixer.music.unpause()
-            print("Playing song")
-        except pygame.error as e:
-            print(f"Error playing song: {e}")
-    else:
-        pse_ply.config(text="▶", font=("Helvetica", 20, "bold"))  
-        pygame.mixer.music.pause()
+    playing = True
+    while playing:
+        pygame.mixer.init()
+        pygame.mixer.music.load(file_path)  # Parameter for file path
+        pygame.mixer.music.play()
+        current_text = pse_ply["text"]
+        if current_text == "▶":
+            # Adjust font size for "Pause" to make it visually similar to "Play"
+            pse_ply.config(text="⏸", font=("Helvetica", 20, "bold"))  
+            try:
+                pygame.mixer.music.unpause()
+                print("Playing song")
+            except pygame.error as e:
+                print(f"Error playing song: {e}")
+        else:
+            pse_ply.config(text="▶", font=("Helvetica", 20, "bold"))  
+            pygame.mixer.music.pause()
 
 def stop_song(pse_ply):
+    playing = False
     pygame.mixer.music.stop() # Vincent make this work pls
     print("Stopped song")
+    return playing
 
 
 def set_volume(volume_slider, volume_label):
