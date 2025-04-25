7ea58a5549713c7e40521d552b62cf1c30d097dd
ILostTheGameAgain <alec.george@ucas-edu.net>
2025-04-25 13:31:58 -0600

    bedugged

--- song_handling.py
+++ song_handling.py
@@ -9,14 +9,13 @@ def song_exists(playlist, song_name):
             return song_name
     return False
 
-def add_song():
-    playlists = load("songs.csv")
+def add_song(playlists):
     # Function to add a song to the playlist
     playlist = song_exists(playlists, input("Enter the name of the playlist: "))
 
     if playlist == False:
         print("Playlist does not exist")
-        playlists = add_song()
+        playlists = add_song(playlists)
         return playlists
 
     filepath = input("Enter the file path of the song to add: ")
@@ -25,15 +24,14 @@ def add_song():
     for song in playlists[playlist]:
         if song[0] == name:
             print("Song already exists in the playlist")
-            playlists = add_song()
+            playlists = add_song(playlists)
             return playlists
     
     playlists[playlist].append([name, filepath])
     return playlists
 
 
-def remove_song():
-    playlists = load("songs.csv")
+def remove_song(playlists):
     # Function to add a song to the playlist
     playlist = song_exists(playlists, input("Enter the name of the playlist: "))
 
@@ -50,30 +48,28 @@ def remove_song():
             return playlists
         
     print("Song does not exist in the playlist")
-    playlists = remove_song()
+    playlists = remove_song(playlists)
     return playlists
 
-def add_playlist():
-    playlists = load("songs.csv")
+def add_playlist(playlists):
 
     playlist_name = input("Enter the name of the playlist: ")
 
     if playlist_name in playlists.keys():
         print("Playlist already exists")
-        playlists = add_playlist()
+        playlists = add_playlist(playlists)
         return playlists
     
     elif playlist_name == "":
         print("Playlist name cannot be empty")
-        playlists = add_playlist()
+        playlists = add_playlist(playlists)
         return playlists
     
     playlists[playlist_name] = []
     return playlists
 
 
-def remove_playlist():
-    playlists = load("songs.csv")
+def remove_playlist(playlists):
 
     playlist_name = input("Enter the name of the playlist: ")
 
@@ -82,7 +78,7 @@ def remove_playlist():
         return playlists
     else:
         print("Playlist does not exist")
-        playlists = remove_playlist()
+        playlists = remove_playlist(playlists)
         return playlists
 
 
